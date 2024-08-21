c832ae770a1d4243dcc5d7f0dafe0b3dd3caf70a
Juan Oroza <juanoroza@candies.dev>
2024-08-21 00:05:25 -0400

    Login menu in NavBar

--- home/views.py
+++ home/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, "home/index.html")
+  if not request.user.is_authenticated:
+    session = False
+    user = ''
+  else:
+    session = True
+    user = request.user
+  return render(request, "home/index.html", { 'session': session, 'user': user})
