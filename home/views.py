cbb52b9ce24b1929294458df21472b6834b263c6
Juan Oroza <juanoroza@candies.dev>
2024-08-20 23:41:10 -0400

    Adding login branch

--- home/views.py
+++ home/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, "home/index.html")
+  user = ''
+  if not request.user.is_authenticated:
+    session = False
+  else:
+    session = True
+    user = request.user
+  return render(request, "home/index.html", {'session': session, 'user_name': user})
